848d1e740a9d4e74ed9b582a2fadae03cb2ea99b
Rithvika Sanil M <rithvikas.cs18@sahyadri.edu.in>
2022-04-21 10:51:09 +0530

    Basic eclipse code

---
+++ GithubProjectExample/src/main/java/com/Basic/code/Addition.java
@@ -0,0 +1,14 @@
+package com.Basic.code;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int a,b,c;
+     a=5;
+     b=4;
+     c=a+b;
+     System.out.println("sum is :"+c);
+	}
+
+}
